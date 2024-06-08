740f221f34e265e311510cb598531df6bbe2ddb3
Abhishek Soni <abhisheksoni099@gmail.com>
2024-06-08 17:11:13 +0530

    Add implementation for the serial dag executor

--- src/main/java/com/abhisheksoni/dagexecutor/SerialDagExecutor.java
+++ src/main/java/com/abhisheksoni/dagexecutor/SerialDagExecutor.java
@@ -1,8 +1,47 @@
 package com.abhisheksoni.dagexecutor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Queue;
+
 import com.abhisheksoni.graph.model.GraphData;
+import com.abhisheksoni.task.BaseTask;
+import com.abhisheksoni.task.Task;
+
 public class SerialDagExecutor implements DagExecutor {
     @Override
     public void execute(GraphData graphData) {
+        Map<String, Integer> dependenciesByTaskName = new HashMap<>();
+        for (Entry<String, ArrayList<Task>> entrySet : graphData.adjacencyGraph().entrySet()) {
+            entrySet.getValue().forEach(taskB -> {
+                String taskAName = entrySet.getKey();
+                String taskBName = ((BaseTask) taskB).getName();
+                dependenciesByTaskName.putIfAbsent(taskAName, 0);
+                dependenciesByTaskName.putIfAbsent(taskBName, 0);
+                dependenciesByTaskName.put(taskBName, dependenciesByTaskName.get(taskBName) + 1);
+            });
+        }
+        Queue<String> queue = new LinkedList<>();
+        for (Entry<String, Integer> taskDependencies : dependenciesByTaskName.entrySet()) {
+            if (taskDependencies.getValue().equals(0)) {
+                queue.add(taskDependencies.getKey());
+            }
+        }
+        while (!queue.isEmpty()) {
+            String currentTaskName = queue.remove();
+            Task currentTask = graphData.tasksByName().get(currentTaskName);
+            currentTask.execute();
+            graphData.adjacencyGraph().get(currentTaskName).forEach(neighbouringTask -> {
+                String neighbouringTaskName = ((BaseTask) neighbouringTask).getName();
+                int neighbouringTaskNewDependency = dependenciesByTaskName.get(neighbouringTaskName) - 1;
+                dependenciesByTaskName.put(neighbouringTaskName, neighbouringTaskNewDependency);
+                if (neighbouringTaskNewDependency == 0) {
+                    queue.add(neighbouringTaskName);
+                }
+            });
+        }
     }
 }
